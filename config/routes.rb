8216adf2cd8bdd9d0eb79d54a4601125b983265f
Andres Martinez <andreseduardo1206@gmail.com>
2020-12-10 22:04:44 -0300

    First commmit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   resources :stories
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
